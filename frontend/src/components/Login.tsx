1bb3082eb348a8b4da673eb92d4e4a8223f899ab
Raziel Aleman <razielali10@gmail.com>
2024-07-20 23:30:41 -0800

    updated UI

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,13 +1,32 @@
-const Login = () => {
-  const handleLogin = () => {
-    window.location.href = "http://localhost:8080/auth/github";
-  };
+import { Box, Button, MantineProvider, Text } from "@mantine/core";
 
-  return (
-    <div>
-      <button onClick={handleLogin}>Login with Github</button>
-    </div>
-  );
+const Login = () => {
+	const handleLogin = () => {
+		window.location.href = "http://localhost:8080/auth/github";
+	};
+
+	return (
+		<MantineProvider>
+			<Box
+				bg="gray.8"
+				style={{
+					padding: "2rem",
+					width: "100%",
+					maxWidth: "40rem",
+					margin: "0 auto",
+					borderRadius: "20px",
+				}}
+			>
+				<Text c="white" p="sm" size="xl">
+					Welcome to GoDo!
+				</Text>
+				{/* <button onClick={handleLogin}>Login with Github</button> */}
+				<Button onClick={handleLogin} m="lg">
+					<Text size="lg">Login with Github</Text>
+				</Button>
+			</Box>
+		</MantineProvider>
+	);
 };
 
 export default Login;
